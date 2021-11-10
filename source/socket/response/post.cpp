0ccb5c5ad811f1b54d7b7038d314d0c2adfb8ad1
RamziNaili <naili.ramzi@outlook.fr>
2021-11-10 20:46:57 +0100

    chunked

--- source/socket/response/post.cpp
+++ source/socket/response/post.cpp
@@ -48,7 +48,7 @@ int	read_body(int client_socket, int client_max_body_size, int file_fd, int cont
 				}
 			}
 		}
-		if (msgsize == 0)
+		if (msgsize == 0) // code d'erreur -> return 1
 			break ;
 		if (msgsize == -1)
 			msgsize = 0;
@@ -59,8 +59,110 @@ int	read_body(int client_socket, int client_max_body_size, int file_fd, int cont
 	return 0;
 }
 
+int	convert_hexa(std::string str) {
+
+	int i = 0;
+	int res = 0;
+	
+	while (str[i] && ((str[i] >= '1' && str[i] <= '9') || (str[i] >= 'a' && str[i] <= 'z')))
+	{
+		res *= 16;
+		res += str[i] < 58 ? str[i] - 48 : str[i] - 87;
+		i++;
+	}
+	return res;
+}
+
+std::string read_chunked_length(int client_socket) {
+	std::string buffer;
+	char buff;
+	int ret = 1;
+
+	while (ret) {
+		ret = read(client_socket, &buff, 1);
+		if (ret == 0)
+			return buffer;
+		if (buff == '\n')
+			return buffer;
+		buffer.insert(buffer.end(), buff);
+	}
+	return buffer;
+}
 
 // read chuncked
+int read_body_chunked(int client_socket, int client_max_body_size, int file_fd)
+{
+	int msgsize = 0;
+	char *buffer;
+
+	try {
+		buffer = new char[BUFFERSIZE];
+	}
+	catch (std::bad_alloc) {
+		std::cerr << "Error: bad_alloc in read_body" << std::endl;
+		return 1;
+	}
+	while (true)
+	{
+		// while (read(client_socket, buffer, 0) < 0)
+			// ;
+		std::string hexa_length = read_chunked_length(client_socket);
+		int nb = convert_hexa(hexa_length);
+		if (nb == 0)
+			break;
+		
+		int client_max = client_max_body_size;
+		if (client_max == 0) // marche pas
+			client_max = nb;
+			
+		for (int total_size = 0; total_size < nb; total_size += msgsize)
+		{
+			msgsize = 0;
+			memset(buffer, 0, BUFFERSIZE);
+			if (total_size + BUFFERSIZE <= nb && total_size + BUFFERSIZE <= client_max)
+			{
+				msgsize = read(client_socket, buffer, BUFFERSIZE);
+			}
+			else
+			{
+				if (total_size + BUFFERSIZE <= nb)
+				{ // si on depasse client max
+					int size = client_max - total_size;
+
+					msgsize = read(client_socket, buffer, size);
+				}
+				else
+				{
+					if (total_size + BUFFERSIZE <= client_max)
+					{ // si on depasse content length
+						int size = nb - total_size;
+
+						msgsize = read(client_socket, buffer, size);
+					}
+					else
+					{
+						int size; // on depasse les content-length et client max body size
+						if (client_max < nb) {
+							size = client_max - total_size;
+						} else
+							size = nb - total_size;
+						msgsize = read(client_socket, buffer, size);
+					}
+				}
+			}
+			if (msgsize == -1)
+				msgsize = 0;
+			buffer[msgsize] = 0;
+			write(file_fd, buffer, msgsize);
+		}
+		char c;
+		read(client_socket, &c, 1);
+		if (c != '\n')
+			std::cerr << "LA GROSSE MERDE" << std::endl;
+	}
+	delete[] buffer;
+	return 0;
+}
 
 void	*method_post(void *arg) {
 	Thread *a = reinterpret_cast<Thread*>(arg);
@@ -98,8 +200,7 @@ void	*method_post(void *arg) {
 		return NULL;
 	}
 
-	if (a->request.get_content_length() >= 0) { // length exist donc body too
-		std::cerr << "tout va bien" << std::endl;
+	if (a->request.get_content_length() >= 0) {
 		if (read_body(a->client_socket, a->server.get_client_size(), fd, a->request.get_content_length())) {
 			send_response(a->request, "", a->client_socket);
 			remove_file(tmp_file.c_str());
@@ -109,7 +210,13 @@ void	*method_post(void *arg) {
 		}
 	}
 	else { // chunked request
-		;
+		if (read_body_chunked(a->client_socket, a->server.get_client_size(), fd)) {
+			send_response(a->request, "", a->client_socket);
+			remove_file(tmp_file.c_str());
+			close(fd);
+			delete a;
+			return NULL;
+		}
 	}
 
 	if (is_cgi(path, location.get_fastcgi_ext()) == true) {
